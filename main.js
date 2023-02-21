37ce74a633777142157ae7789e449ef478d462c6
MrCyberlord <amanjain2691@gmail.com>
2023-02-21 14:49:14 +0530

    added event listener

--- main.js
+++ main.js
@@ -5,22 +5,27 @@ let users = document.getElementById("users");
 const endpointId = "28554357ecae45359ea888f37169595e";
 const serverLink = `https://crudcrud.com/api/${endpointId}/user`;
 
-axios(serverLink)
-  .then((res) => {
-    const userJSON = res.data;
-    for (let i = 0; i < userJSON.length; i++) {
-      userDisplay(userJSON[i]);
-    }
-  })
-  .catch((err) => console.log(err));
+addEventListener("DOMContentLoaded", () => {
+  //loading from server
+  axios(serverLink)
+    .then((res) => {
+      const userJSON = res.data;
+      for (let i = 0; i < userJSON.length; i++) {
+        addUserToDisplay(userJSON[i]);
+      }
+    })
+    .catch((err) => console.log(err));
+});
 
 let delBtn = document.getElementsByClassName("del");
 let editBtn = document.getElementsByClassName("edit");
 
+//adding event listeners to edit and delete button of all list
 for (let i = 0; i < delBtn.length; i++) {
   delBtn[i].addEventListener("click", deleteLi);
   editBtn[i].addEventListener("click", editLi);
 }
+
 form.addEventListener("submit", submitFn);
 
 function deleteLi(e) {
@@ -31,7 +36,6 @@ function deleteLi(e) {
     .catch((err) => console.log(err));
   e.path[1].remove();
 }
-
 function editLi(e) {
   email.value = e.path[1].textContent.slice(0, -5).split(" : ")[1];
   name.value = e.path[1].textContent.slice(0, -5).split(" : ")[0];
@@ -44,8 +48,10 @@ function submitFn(e) {
     name: name.value,
     email: email.value,
   };
-  userDisplay(user);
 
+  addUserToDisplay(user);
+
+  //uploading the new user details to server
   axios
     .post(serverLink, user)
     .then((res) => console.log(res))
@@ -54,20 +60,28 @@ function submitFn(e) {
   email.value = "";
 }
 
-function userDisplay(user) {
+function createDeleteButton() {
+  let deleteButton = document.createElement("button");
+  deleteButton.textContent = "X";
+  deleteButton.classList.add("del");
+  deleteButton.addEventListener("click", deleteLi);
+  return deleteButton;
+}
+
+function createEditButton() {
+  let editButton = document.createElement("button");
+  editButton.textContent = "edit";
+  editButton.classList.add("edit");
+  editButton.addEventListener("click", editLi);
+  return editButton;
+}
+
+function addUserToDisplay(user) {
   let li = document.createElement("li");
   li.appendChild(document.createTextNode(`${user.name} : ${user.email}`));
-  li.appendChild(document.createTextNode(`${user.name} : ${user.email}`));
   li.setAttribute("id", user._id);
-  let delBtn = document.createElement("button");
-  delBtn.textContent = "X";
-  delBtn.classList.add("del");
-  li.appendChild(delBtn);
-  delBtn.addEventListener("click", deleteLi);
-  let editBtn = document.createElement("button");
-  editBtn.textContent = "edit";
-  editBtn.classList.add("edit");
-  li.appendChild(editBtn);
-  editBtn.addEventListener("click", editLi);
+
+  li.appendChild(createDeleteButton());
+  li.appendChild(createEditButton());
   users.appendChild(li);
 }
